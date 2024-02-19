5a74de696f7ab3f09e1defc4afb99b09976e7d13
haniehhoseini <hecallsmehni27@gamil.com>
2024-02-19 15:07:58 +0330

    nested menubar

--- nestedMenu/src/app/app.component.ts
+++ nestedMenu/src/app/app.component.ts
@@ -4,103 +4,18 @@ import { MenuItemComponent } from './menu-item/menu-item.component';
 import { VERSION } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { NavItem } from './nav-item';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule} from '@angular/material/menu';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet , MenuItemComponent , CommonModule  , RouterModule , MatIconModule , MatMenuModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  version = VERSION;
-  navItems: any[] = [
-    {
-      displayName: 'AngularMix',
-      iconName: 'close',
-      children: [
-        {
-          displayName: 'Speakers',
-          iconName: 'group',
-          children: [
-            {
-              displayName: 'Michael Prentice',
-              iconName: 'person',
-              route: 'michael-prentice',
-              children: [
-                {
-                  displayName: 'Delight your Organization',
-                  iconName: 'star_rate',
-                  route: 'material-design'
-                }
-              ]
-            },
-            {
-              displayName: 'Stephen Fluin',
-              iconName: 'person',
-              route: 'stephen-fluin',
-              children: [
-                {
-                  displayName: 'What\'s up with the Web?',
-                  iconName: 'star_rate',
-                  route: 'what-up-web'
-                }
-              ]
-            },
-            {
-              displayName: 'Mike Brocchi',
-              iconName: 'person',
-              route: 'mike-brocchi',
-              children: [
-                {
-                  displayName: 'My ally, the CLI',
-                  iconName: 'star_rate',
-                  route: 'my-ally-cli'
-                },
-                {
-                  displayName: 'Become an Angular Tailor',
-                  iconName: 'star_rate',
-                  route: 'become-angular-tailer'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          displayName: 'Sessions',
-          iconName: 'speaker_notes',
-          children: [
-            {
-              displayName: 'Delight your Organization',
-              iconName: 'star_rate',
-              route: 'material-design'
-            },
-            {
-              displayName: 'What\'s up with the Web?',
-              iconName: 'star_rate',
-              route: 'what-up-web'
-            },
-            {
-              displayName: 'My ally, the CLI',
-              iconName: 'star_rate',
-              route: 'my-ally-cli'
-            },
-            {
-              displayName: 'Become an Angular Tailor',
-              iconName: 'star_rate',
-              route: 'become-angular-tailer'
-            }
-          ]
-        },
-        {
-          displayName: 'Feedback',
-          iconName: 'feedback',
-          route: 'feedback'
-        }
-      ]
-    }
-  ];
+
 }
--- nestedMenu/src/app/menu-item/menu-item.component.ts
+++ nestedMenu/src/app/menu-item/menu-item.component.ts
@@ -1,6 +1,5 @@
 import { Component , Input, OnInit, ViewChild} from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { NavItem } from '../nav-item';
 import { ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,14 +14,50 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './menu-item.component.html',
   styleUrl: './menu-item.component.scss'
 })
-export class MenuItemComponent implements OnInit{
-  @Input() items!: NavItem[];
-  @ViewChild('childMenu') public childMenu!: ElementRef;
-  
+export class MenuItemComponent {
+  menuItems = [
+    {
+      label: 'Menu 1',
+      subItems: [
+        { label: 'Item 1.1', action: () => console.log('Clicked Item 1.1') },
+        { label: 'Item 1.2', action: () => console.log('Clicked Item 1.2') }
+      ]
+    },
+    {
+      label: 'Menu 2',
+      subItems: [
+        { label: 'Item 2.1', action: () => console.log('Clicked Item 2.1') },
+        { label: 'Item 2.2', action: () => console.log('Clicked Item 2.2') }
+      ]
+    },
+    {
+      label: 'Menu 3',
+      subItems: [
+        { label: 'Item 3.1', action: () => console.log('Clicked Item 3.1') },
+        { label: 'Item 3.2', action: () => console.log('Clicked Item 3.2') }
+      ]
+    },
+    {
+      label: 'Menu 4',
+      subItems: [
+        { label: 'Item 4.1', action: () => console.log('Clicked Item 4.1') },
+        {
+          label: 'Item 4.2',
+          subItems: [
+            { label: 'Subitem 4.2.1', action: () => console.log('Clicked Subitem 4.2.1') },
+            { label: 'Subitem 4.2.2', action: () => console.log('Clicked Subitem 4.2.2') }
+          ]
+        }
+      ]
+    },
+    // Add more menus as needed
+  ];
 
-  constructor(public router: Router) {
-  }
+  @Input() menuItem: any; // Assuming menuItem has a 'label' and 'subItems' property
 
-  ngOnInit() {
+  handleItemClick(subItem: any): void {
+    if (subItem && subItem.action && typeof subItem.action === 'function') {
+      subItem.action();
+    }
   }
 }
